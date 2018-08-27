f8d4463ec664f17f131689927dcb0955aa4086cf
Darrell Martin <dwmkv9@mail.missouri.edu>
2018-08-27 14:03:09 -0500

    add files for HelloWorld project IT4500

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func sayHello(_ sender: Any) {
+        welcomeLabel.text = "Hello World!"
+    }
+    
+    @IBAction func clearLabel(_ sender: Any) {
+        welcomeLabel.text = " "
+    }
+    
 }
 
